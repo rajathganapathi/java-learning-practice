b76d70359e339e4d71f25d098cd5614192491351
unknown <rajath.ganapathi55@gmail.com>
2026-06-18 16:26:32 +0530

    Adding newly learnt math classes for future reference

---
+++ 07-Math-Class-Intro-to-Math-Classes/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String args[]){
+//        System.out.println(Math.PI);
+//        System.out.println(Math.E);
+        double result;
+
+        result = Math.pow(2,4);
+        result = Math.abs(-8);
+        result = Math.sqrt(81);
+        result = Math.round(5.37);
+        result = Math.ceil(3.9);
+        result = Math.floor(6.7);
+        result = Math.max(23,90);
+        result = Math.min(23,90);
+
+        System.out.println(result);
+
+    }
+}
